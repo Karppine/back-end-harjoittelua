333ccbba3af7b510e997884e78340d2dfbb9851e
Leevi Karppinen <ljkarp@utu.fi>
2024-05-05 17:31:57 +0300

    osan 3 harjoituksia

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+require('dotenv').config()
+const Note = require('./models/note')
+
+
 app.use(express.static('dist'))
 app.use(cors())
+
+
 
 let notes = [
     {
@@ -42,59 +48,87 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!!!<h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
 
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end()
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+  
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+
+    
+    note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    res.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = notes.lengt > 0 ? 0 : Math.max(...notes.map(n => n.id))
-    console.log("notes pituus", notes.length)
-    console.log("max id: ", maxId)
-    return maxId + 1    
-}
-
-app.post('/api/notes', (req, res) => {
-    const body = req.body
-
-    if (!body.content) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
-
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
     const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
+      content: body.content,
+      important: body.important,
     }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
 
-    console.log(note)
-    notes = notes.concat(note)
-    res.json(note)
-})
 
   
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+  
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
