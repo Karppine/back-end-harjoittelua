c95233f082a78b14c76bb46fdbe07bd27b985292
Leevi Karppinen <ljkarp@utu.fi>
2024-05-17 10:04:48 +0300

    4c

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,14 +1,9 @@
+const User = require('../models/user')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-// notesRouter.get('/', (request, response) => {
-//   Note.find({}).then(notes => {
-//     response.json(notes)
-//   })
-// })
-
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', {username: 1, name: 1})
   response.json(notes)
 })
 
@@ -19,45 +14,29 @@ notesRouter.get('/:id', async (request, response) => {
   } else {
     response.status(404).end()
   }
-  
-  // Note.findById(request.params.id)
-  //   .then(note => {
-  //     if (note) {
-  //       response.json(note)
-  //     } else {
-  //       response.status(404).end()
-  //     }
-  //   })
-  //   .catch(error => next(error))
 })
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    // important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
-  
-  // note.save()
-  //   .then(savedNote => {
-  //     response.status(201).json(savedNote)
-  //   })
-  //   .catch(error => next(error))
 })
 
 notesRouter.delete('/:id', async (request, response) => {
   await Note.findByIdAndDelete(request.params.id)
   response.status(204).end()
-
-  // Note.findByIdAndDelete(request.params.id)
-  //   .then(() => {
-  //     response.status(204).end()
-  //   })
-  //   .catch(error => next(error))
 })
 
 notesRouter.put('/:id', (request, response, next) => {
